b6a8d3b745bc7bd5248c599e1dcb8d4b07e4e76d
clincha <angus.clinch@gmail.com>
2021-02-25 22:31:40 +0000

    it does what it says on the tin

--- hitter/urls.py
+++ hitter/urls.py
@@ -5,4 +5,5 @@ from hitter import views
 app_name = 'hitter'
 urlpatterns = [
     path('welcome/', views.welcome),
+    path('make-sheet/', views.make_sheet, name='make-sheet')
 ]
--- hitter/views.py
+++ hitter/views.py
@@ -4,17 +4,34 @@ import gspread
 from django.shortcuts import render
 
 from sheeter.models import UserGoogle
+from starling.models import Starling
 
 
 def welcome(request):
     context = {}
     if 'Google' in request.session.keys():
         context['google_login'] = True
-        user_google = UserGoogle.objects.filter(id=request.session['Google']).only().first()
-        gc = gspread.authorize(pickle.loads(user_google.credentials))
-        print(gc.open("Finance").sheet1.cell(1, 1).value)
 
     if 'Starling' in request.session.keys():
         context['starling_login'] = True
 
     return render(request, 'hitter/index.html', context)
+
+
+def make_sheet(request):
+    context = {}
+    if 'Google' in request.session.keys():
+        context['google_login'] = True
+
+    if 'Starling' in request.session.keys():
+        context['starling_login'] = True
+
+    if context['google_login'] and context['starling_login']:
+        user_google = UserGoogle.objects.filter(id=request.session['Google']).only().first()
+        user_starling = Starling.objects.filter(id=request.session['Starling']).only().first()
+
+        gc = gspread.authorize(pickle.loads(user_google.credentials))
+
+        gc.create("test").sheet1.append_rows(user_starling.get_full_transaction_history())
+
+    return welcome(request)
