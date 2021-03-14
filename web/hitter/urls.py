5ceb667c1090585e624f2aeeec402ec82ae2ad9b
clincha <angus.clinch@gmail.com>
2021-03-14 12:21:36 +0000

    remove /

--- web/hitter/urls.py
+++ web/hitter/urls.py
@@ -4,7 +4,7 @@ from hitter import views
 
 app_name = 'hitter'
 urlpatterns = [
-    path('/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('welcome/', views.welcome, name='welcome'),
     path('make-sheet/', views.make_sheet, name='make-sheet')
 ]
