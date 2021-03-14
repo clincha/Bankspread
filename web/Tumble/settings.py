160a7bf019b01e9540a5abba66197cb23b500bea
clincha <angus.clinch@gmail.com>
2021-03-14 10:52:15 +0000

    Deployment key

--- web/Tumble/settings.py
+++ web/Tumble/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q+57b2%6+$2zgh$c_=h=lbwvncl(ozb%-oyy-_61pj1^9+&z=a'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
