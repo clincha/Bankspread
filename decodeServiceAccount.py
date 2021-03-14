63c28d30cf9e2ed9b31f5096148ef7f95f54df0c
clincha <angus.clinch@gmail.com>
2021-03-14 12:16:12 +0000

    where is the file tho?

--- decodeServiceAccount.py
+++ decodeServiceAccount.py
@@ -5,6 +5,6 @@ base64_message = os.getenv('SERVICE_ACCOUNT')
 base64_bytes = base64_message.encode('ascii')
 message_bytes = base64.b64decode(base64_bytes)
 
-with open("service_account.json", 'wb') as file:
+with open("./web/sheeter/service_account.json", 'wb') as file:
     file.write(message_bytes)
     file.close()
